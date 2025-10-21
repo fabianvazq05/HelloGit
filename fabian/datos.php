fb89f25f60592b918e20e3c5bb2387910f88f5c4
fabianvazq05 <vazquezfabian772@gmail.com>
2025-10-21 11:46:15 -0600

    Primer commit

---
+++ fabian/datos.php
@@ -0,0 +1,24 @@
+<?php
+$nombre_get="Aaron"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOLAAAA</h1>
+    <form action="#" method="get">
+        <label for="">$nombre_get
+            <input type="text" name="nombre" id="">
+        <br>
+        </label>
+        <label for="">primer apellido</label>
+            <input type="text" name="primerApellido" id="">
+        <button type="submit">Enviar</button>    
+    </form>
+</body>
+</html>
